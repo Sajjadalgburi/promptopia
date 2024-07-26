f2a11d8ebebdfc4910201bb043add668f2cb462f
Sajjadalgburi <dfgsajjad@gmail.com>
2024-07-26 18:03:52 -0400

    initilizing the navbar when the user is signed in

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,6 +8,8 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link className="flex gap-2 flex-center" href={"/"}>
@@ -18,7 +20,35 @@ const Nav = () => {
           height={30}
           className="object-contain"
         />
+        <p className="logo_text">Promptopia </p>
       </Link>
+
+      {/* Desktop Nabigation */}
+      <div className="sm:flex hidden ">
+        {isUserLoggedIn ? (
+          <div className=" flex gap-3 md:gap-5">
+            <Link href={"/create-prompt"} className="black_btn">
+              create Post
+            </Link>
+
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+
+            <Link href={"/profile"}>
+              <Image
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+                src={"/assets/images/logo.svg"}
+              />
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
   );
 };
