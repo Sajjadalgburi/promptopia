165c2661f4c0c6a99fffaedd915de310747f3edc
Sajjadalgburi <dfgsajjad@gmail.com>
2024-07-25 20:27:59 -0400

    importing nesscary files/modules

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,14 @@
+// this will be a client components because we are utlizing
+
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
 const Nav = () => {
-  return <div></div>;
+  return <nav className="flex-between w-full mb-16 pt-3"></nav>;
 };
 
 export default Nav;
