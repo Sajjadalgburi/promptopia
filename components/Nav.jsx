908915e85a28ff85cec4811bb0b753519eab4c09
Sajjadalgburi <dfgsajjad@gmail.com>
2024-07-26 15:09:03 -0400

    Styling the image within the navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,7 +8,19 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  return <nav className="flex-between w-full mb-16 pt-3"></nav>;
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link className="flex gap-2 flex-center" href={"/"}>
+        <Image
+          src={"./assets/images/logo.svg"}
+          alt="Promptopia Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+      </Link>
+    </nav>
+  );
 };
 
 export default Nav;
