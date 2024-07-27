ffd32c21a7abcc1e488eeb1a91a39656089d1e9a
Sajjadalgburi <dfgsajjad@gmail.com>
2024-07-27 13:06:51 -0400

    wrote the function which will essentiall create the post by sending the response to the backend

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -5,7 +5,6 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 import Form from "@components/Form";
-import { set } from "mongoose";
 
 const CreatePrompt = () => {
   const [subitting, setSubmitting] = useState(false);
@@ -15,7 +14,33 @@ const CreatePrompt = () => {
     tag: "",
   });
 
-  const createPost = async () => {};
+  const session = useSession();
+  const router = useRouter();
+
+  const createPost = async (e) => {
+    e.preventDefault(); // prevent the normal behaviour of reloading once the form is submitted
+
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
