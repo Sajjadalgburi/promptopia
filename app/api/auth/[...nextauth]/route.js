4225b390e875cb49a2e6655780abf3b91afab348
Sajjadalgburi <dfgsajjad@gmail.com>
2024-07-27 11:30:35 -0400

    the sign in callback will create a user if they dont exit in the DB

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+
+import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
@@ -9,21 +11,36 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
 
-  async session({ session }) {},
+      return session;
+    },
+    async signIn({ account, profile, user, credentials }) {
+      try {
+        await connectToDB();
 
-  // serverless => lambda - meaning it will only open up once it is called
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
-      // check if the user exists
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
 
-      // if not, we must created the user
-    } catch (error) {
-      console.error(error);
-      return false; // this indicated that the login has faild
-    }
+        return true;
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false;
+      }
+    },
   },
 });
 
