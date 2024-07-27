f9c9288fb71af20e1e520a3194c21b6d91b583a4
Sajjadalgburi <dfgsajjad@gmail.com>
2024-07-27 10:01:01 -0400

    creating the custom provider

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,5 +1,10 @@
-const Provider = () => {
-  return <div></div>;
+// this component will pass/'provide' the session to the other components
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+
+const Provider = ({ children, session }) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 
 export default Provider;
