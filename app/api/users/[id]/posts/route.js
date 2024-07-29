5af610aa81387c4f588c5a10d837234ab819c2b0
Sajjadalgburi <dfgsajjad@gmail.com>
2024-07-29 11:21:33 -0400

    grab the users posts by they given id

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,19 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDB();
+
+    const prompt = await Prompt.find({
+      creator: params.id,
+    }).populate("creator");
+
+    return new Response(JSON.stringify(prompt), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response("Could not fetch the prompts", { status: 500 });
+  }
+};
