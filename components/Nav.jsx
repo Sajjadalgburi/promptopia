7966bb6b6ed447cfe7f274a94db21b357d566729
Sajjadalgburi <dfgsajjad@gmail.com>
2024-07-29 11:22:07 -0400

    fixing many errors

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,61 +1,55 @@
-// this will be a client components because we are utlizing
-
 "use client";
 
 import Link from "next/link";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
   const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviderss = async () => {
-      const response = await getProviders();
-
-      setProviders(response);
-    };
-
-    setProviderss();
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
   }, []);
-
-  const [toggleDropDown, setToggleDropDown] = useState(false);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
-      <Link className="flex gap-2 flex-center" href={"/"}>
+      <Link href="/" className="flex gap-2 flex-center">
         <Image
-          src={"./assets/images/logo.svg"}
-          alt="Promptopia Logo"
+          src="/assets/images/logo.svg"
+          alt="logo"
           width={30}
           height={30}
           className="object-contain"
         />
-        <p className="logo_text">Promptopia </p>
+        <p className="logo_text">Promptopia</p>
       </Link>
 
-      {/* Desktop Nabigation */}
-      <div className="sm:flex hidden ">
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden">
         {session?.user ? (
-          <div className=" flex gap-3 md:gap-5">
-            <Link href={"create-prompt"} className="black_btn">
-              Create Prompt
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
             </Link>
 
             <button type="button" onClick={signOut} className="outline_btn">
               Sign Out
             </button>
 
-            <Link href={"profile"}>
+            <Link href="/profile">
               <Image
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
                 alt="profile"
-                src={session?.user.image}
               />
             </Link>
           </div>
@@ -64,59 +58,56 @@ const Nav = () => {
             {providers &&
               Object.values(providers).map((provider) => (
                 <button
-                  onClick={() => signIn(provider.id)}
                   type="button"
                   key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
                   className="black_btn"
                 >
-                  Sign In
+                  Sign in
                 </button>
               ))}
           </>
         )}
       </div>
-      {/* Mobile Nabigation */}
+
+      {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
         {session?.user ? (
           <div className="flex">
-            {" "}
             <Image
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
               alt="profile"
-              src={session?.user.image}
-              // changing the state using a concise way! (does the opposite of what the state was before)
-              onClick={() => {
-                setToggleDropDown((prev) => !prev);
-              }}
+              onClick={() => setToggleDropdown(!toggleDropdown)}
             />
-            {toggleDropDown && (
+
+            {toggleDropdown && (
               <div className="dropdown">
                 <Link
-                  href={"profile"}
+                  href="/profile"
                   className="dropdown_link"
-                  // when the user clicks, it will redirect to '/profile' and set the state of the toggle to false to hide it
-                  onClick={() => setToggleDropDown(false)}
+                  onClick={() => setToggleDropdown(false)}
                 >
                   My Profile
                 </Link>
                 <Link
-                  href={"create-prompt"}
+                  href="/create-prompt"
                   className="dropdown_link"
-                  // when the user clicks, it will redirect to '/profile' and set the state of the toggle to false to hide it
-                  onClick={() => setToggleDropDown(false)}
+                  onClick={() => setToggleDropdown(false)}
                 >
-                  Create Post
+                  Create Prompt
                 </Link>
-
                 <button
                   type="button"
                   onClick={() => {
-                    setToggleDropDown(false);
+                    setToggleDropdown(false);
                     signOut();
                   }}
-                  className="black_btn mt-5 w-full"
+                  className="mt-5 w-full black_btn"
                 >
                   Sign Out
                 </button>
@@ -128,12 +119,14 @@ const Nav = () => {
             {providers &&
               Object.values(providers).map((provider) => (
                 <button
-                  onClick={() => signIn(provider.id)}
                   type="button"
                   key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
                   className="black_btn"
                 >
-                  Sign In
+                  Sign in
                 </button>
               ))}
           </>
